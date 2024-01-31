fbec4d5fd2f2c3fb56135825d3147ace4d1b77dc
donnyha <donny.dh.ha@gmail.com>
2024-01-31 17:12:31 -0600

    applied fonts

--- wp-content/themes/goldenboy/header.php
+++ wp-content/themes/goldenboy/header.php
@@ -6,6 +6,9 @@
     <?php 
       wp_head();
     ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Roboto+Slab:wght@400;500;600;700&display=swap" rel="stylesheet">
   </head>
   <body>
     <!-- Desktop Nav -->
